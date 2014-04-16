82504b96ebd5cf7be95565bd249e76dadf3426fe
Paul <Paul.kinross.gray@gmail.com>
2014-04-16 14:25:33 +0100

    Distance Service #3

--- demo/app/demo.mdl.js
+++ demo/app/demo.mdl.js
@@ -1,52 +1,69 @@
 ﻿angular.module('demo', ['ngLatLng'])
-    .controller('demoCtrl', ['$scope', 'placesLatLng', 'latLngCurrentPosition', function ($scope, placesLatLng, latLngCurrentPosition) {
+    .controller('demoCtrl', [
+        '$scope',
+        'placesLatLng',
+        'latLngCurrentPosition',
+        'latLngDistance', function ($scope, placesLatLng, latLngCurrentPosition, latLngDistance) {
 
-        $scope.currentPosition = {};
-        $scope.places = placesLatLng.getPlaces();
+            $scope.currentPosition = {};
+            $scope.places = placesLatLng.getPlaces();
 
-        latLngCurrentPosition().then(function (result) {
-            $scope.currentPosition.lat = result.coords.latitude;
-            $scope.currentPosition.lng = result.coords.longitude
-        });
-    }])
+            latLngCurrentPosition().then(function (result) {
+                $scope.currentPosition.lat = result.coords.latitude;
+                $scope.currentPosition.lng = result.coords.longitude
+
+                console.log($scope.places);
+
+                for (var i = 0; i < $scope.places.length; i++) {
+
+                    var distance = latLngDistance(
+                        result.coords.latitude,
+                        result.coords.longitude,
+                        $scope.places[i].lat,
+                        $scope.places[i].lng)
+
+                    $scope.places[i].distance = distance;
+
+                };
+
+
+            });
+        }])
     .service('placesLatLng', [function () {
 
         this.getPlaces = function () {
- 
+
             return this.places;
 
         };
 
 
-        this.places = {
-            cities: {
-                Munich: {
-                    city: "Munich",
-                    lat: 48.133000000000003,
-                    lng: -10.433,
-                },
-                London: {
+        this.places = [{
+            city: "Munich",
+            lat: 48.133000000000003,
+            lng: -10.433,
+        },
+                {
                     city: "London",
                     lat: 51.303000000000003,
                     lng: 0.732,
                 },
-                NewYork: {
+                {
                     city: "New York",
                     lat: 40.425100000000003,
                     lng: 73.98,
                 },
-                CapeTown: {
+                {
                     city: "Cape Town",
                     lat: -33.92486,
                     lng: 18.4240552,
                 },
-                HongKong: {
+                {
                     city: "Hong Kong",
                     lat: 22.302698,
                     lng: 114.160501,
                 },
-            }
-        }
+        ]
 
 
 
--- src/features/distance/distance.svc.js
+++ src/features/distance/distance.svc.js
@@ -1,5 +1,23 @@
 ﻿ngLatLng.service('latLngDistance', [function () {
+    
+    return function (lat1, lon1, lat2, lon2) {
 
+        var radiusOfPlanet = 6371;
+        var dLat = deg2rad(lat2 - lat1);  
+        var dLon = deg2rad(lon2 - lon1);
+        var a =
+          Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+          Math.sin(dLon / 2) * Math.sin(dLon / 2)
+        ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var distance = radiusOfPlanet * c; 
+        return distance;
+    }
+
+    function deg2rad(deg) {
+        return deg * (Math.PI / 180)
+    }
     
 
 }]);
